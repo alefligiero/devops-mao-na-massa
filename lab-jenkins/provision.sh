6587a43ce4f43712d9e8cc29f3e0e6e3de74412f
Alef Ligiero <alefligieroagb@gmail.com>
2022-10-26 14:13:14 -0300

    add jenkins with sonarqube

---
+++ lab-jenkins/provision.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+yum install epel-release -y
+yum install wget git -y
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install java-11-openjdk-devel -y
+yum install jenkins -y
+systemctl daemon-reload
+systemctl start jenkins
+systemctl enable jenkins
+
+### docker installation
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
+usermod -aG docker jenkins
+
+# sonar scanner installation
+
+yum install wget unzip -y
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip
+unzip sonar-scanner-cli-4.7.0.2747-linux.zip -d /opt/
+mv /opt/sonar-scanner-4.7.0.2747-linux /opt/sonar-scanner
+chown -R jenkins:jenkins /opt/sonar-scanner
+echo 'export PATH=$PATH:/opt/sonar-scanner/bin' | sudo tee -a /etc/profile
+curl -fsSL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install nodejs -y
